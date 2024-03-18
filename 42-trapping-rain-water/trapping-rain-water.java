a2055970466ea16727c652f2a6a6a254f58fb123
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-03-18 19:53:41 +0530

    Time: 688 ms (5.01%) | Memory: 46.5 MB (25.68%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -2,7 +2,6 @@ class Solution {
     public int trap(int[] height) {
         int n = height.length;
         int res = 0;
-
         // Let us iterate through the all height from second to secondlast height
         for(int i = 1; i < n-1; i++){
             // We are finding the maximum height on left, first we are intialising left
@@ -15,7 +14,6 @@ class Solution {
             for(int j = i + 1; j < n; j++){
                 right = Math.max(right, height[j]);
             }
-
             // We are adding the result with the minimum of left and right and subtracting the height of the current bar/index
             res += Math.min(left, right) - height[i];
         }
