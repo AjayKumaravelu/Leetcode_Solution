196488bc31cdb044ea44d5d2898dd2c9fab6c6b7
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-03-25 12:48:22 +0530

    Time: 7 ms (72.57%) | Memory: 44.9 MB (27.95%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -14,16 +14,19 @@ class Solution {
             return root;
         }
         
-        
+        // Recursively search for p and q in left and right subtrees
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
-
+        
+        // If both left and right subtrees return non-null, root is LCA
         if(left != null && right != null){
             return root;
         }
+        // LCA found in left subtree
         else if(left != null){
             return left;
         }
+        // LCA found in right subtree
         else{
             return right;
         }
