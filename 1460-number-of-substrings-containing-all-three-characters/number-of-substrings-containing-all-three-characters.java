306eac423553aa2fbced89b522d79ce2a4a5d6dc
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-03-27 16:40:46 +0530

    Time: 12 ms (82.14%) | Memory: 44.6 MB (51.13%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -10,7 +10,7 @@ class Solution {
             // Update the last seen index of the character at position 'i'
             lastseen[s.charAt(i) - 'a'] = i;
             // Check if all characters 'a', 'b', and 'c' have been seen at least once
-            if(lastseen[0] != -1 && lastseen[0] != -1 && lastseen[0] != -1){
+            if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){
                 // Find the minimum last seen index among characters 'a', 'b', and 'c'
                 count += (1 + Math.min(lastseen[0], Math.min(lastseen[1],lastseen[2])));
             }
