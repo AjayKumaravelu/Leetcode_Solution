e624fd5e6e98a7e3dfe898c260ea6fc1f2bb6c32
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-03-28 12:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (58.37%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,4 +1,4 @@
-/**
+ /**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -9,44 +9,57 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode start, ListNode end){
-        ListNode current = start.next;
-        ListNode prev = start;
-        ListNode first = current;
-
-        while(current != end){
-            ListNode temp = current.next;
-            current.next = prev;
-            prev = current;
-            current = temp;
+    
+    // Method to reverse a sublist of the linked list from start to end (excluding end)
+    public ListNode reverse(ListNode start, ListNode end) {
+        ListNode current = start.next; // Current node to be reversed
+        ListNode prev = start; // Previous node of current
+        ListNode first = current; // Save the first node of the reversed sublist
+        
+        // Reverse the sublist from start to end
+        while (current != end) {
+            ListNode temp = current.next; // Save the next node
+            current.next = prev; // Reverse the pointer direction
+            prev = current; // Move prev to current
+            current = temp; // Move current to next
         }
 
+        // Connect the start of the sublist to the last node of the reversed sublist
         start.next = prev;
+        // Connect the first node of the reversed sublist to the node after the end
         first.next = current;
+        
+        // Return the first node of the reversed sublist
         return first;
     }
 
+    // Method to reverse the linked list in groups of size k
     public ListNode reverseKGroup(ListNode head, int k) {
-        if(head == null && k < 1){
+        if (head == null || k < 1) {
             return head;
         }
-        ListNode dNode = new ListNode(0);
-        dNode.next = head;
+        
+        // Create a dummy node and connect it to the head
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
 
-        ListNode start = dNode;
-        ListNode end = head;
-        int count = 0;
-
-        while(end != null){
-            count++;
-            if(count % k == 0){
-                start = reverse(start,end.next);
-                end = start.next;
-            }
-            else{
-                end = end.next;
+        ListNode start = dummyNode; // Points to the start of each sublist
+        ListNode end = head; // Points to the end of each sublist
+        int count = 0; // Counter to keep track of nodes processed
+        
+        // Traverse the list
+        while (end != null) {
+            count++; // Increment the counter
+            if (count % k == 0) { // If the count is a multiple of k
+                // Reverse the sublist from start to end
+                start = reverse(start, end.next);
+                end = start.next; // Move end to the start of the next sublist
+            } else {
+                end = end.next; // Move end to the next node
             }
         }
-        return dNode.next;
+        
+        // Return the modified list
+        return dummyNode.next;
     }
 }
