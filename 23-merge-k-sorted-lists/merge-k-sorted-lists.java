dbb6503e982e3e1232c808f542aa486bb3629273
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-03-28 11:14:55 +0530

    Time: 5 ms (40.99%) | Memory: 44.8 MB (11.61%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,22 +10,31 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        // Creating the priority queue with Integer and Node
         PriorityQueue<Pair<Integer,ListNode>> pq = new PriorityQueue<>((a,b)-> a.getKey()-b.getKey());
+        // Traversing the list and adding it to priority Queue
         for(int i = 0 ; i < lists.length ; i++){
             if(lists[i] != null){
+                // Adding integer and node to the Queue
                 pq.add(new Pair<>(lists[i].val, lists[i]));
             }
         }
 
+        //Creating a Dummy Node
         ListNode dNode = new ListNode(-1);
+        // Assigning temp to dNode
         ListNode temp = dNode;
         while(!pq.isEmpty()){
+            // We are getting the min value from priority Queue(min-heap)
             Pair<Integer, ListNode> pair = pq.poll();
             ListNode node = pair.getValue();
             if(node.next != null){
+                // Adding the next Value to the priority Queue
                 pq.add(new Pair<>(node.next.val, node.next));
             }
+            // We are pointing the temp->next to that node
             temp.next = node;
+            // We are moving temp to temp->next
             temp = temp.next;
         }
         return dNode.next;
