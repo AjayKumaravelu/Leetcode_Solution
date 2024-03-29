35f02211bdd76df49492e565f65053543930f3d7
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-03-29 19:02:38 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (72.23%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -21,12 +21,15 @@ class Solution {
     private TreeNode prev;
 
     private void inorder(TreeNode root){
+        // Base condition
         if(root == null){
             return;
         }
         inorder(root.left);
-
+        
+        // We are checking the condition to be inorder in which root.val > prev.val if it is false we are going into this if loop
         if(prev != null && root.val < prev.val){
+            // Checking whether the first is null 
             if(first == null){
                 first = prev;
                 middle = root;
@@ -41,15 +44,18 @@ class Solution {
     }
 
     public void recoverTree(TreeNode root) {
+        // Initialising the first,middle,last
         first = middle = last = null;
         prev = new TreeNode(Integer.MIN_VALUE);
         inorder(root);
-
+        
+        // Swapping first and last
         if(first != null && last != null){
             int temp = first.val;
             first.val = last.val;
             last.val = temp;
         }
+        //Swapping first and middle ---> when the swapping in adjacent nodes
         else if(first != null && middle != null){
             int temp = first.val;
             first.val = middle.val;
