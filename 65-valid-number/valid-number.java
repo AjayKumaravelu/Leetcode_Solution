91a3faacd7658dc46984f582b38678d4ef5ddcef
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-04-20 13:26:57 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (38.45%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,48 +1,4 @@
 class Solution {
-    /*public boolean isNumber(String s) {
-        if(s == null || s.length() == 0) 
-            return false;
-        int start = 0;
-        // handle the space at the front
-        while(start < s.length() && s.charAt(start) == ' ')
-            start++;
-        if(start == s.length())
-            return false;
-        if(s.charAt(start) == '+' || s.charAt(start) == '-')
-            start++;
-        boolean hasExp = false;
-        boolean hasNum = false;
-        boolean hasDot = false;
-        while(start < s.length()) {
-            char c = s.charAt(start);
-            // handle the space in the end
-            if(c == ' ') {
-                while(start < s.length() && s.charAt(start) == ' ')
-                    start++;
-                if(start == s.length())
-                    break;
-                return false;
-            } else if (c >= '0' && c <= '9') {
-                hasNum = true;
-            } else if(c == '+' || c == '-') {
-                if(s.charAt(start - 1) != 'e')
-                    return false;
-            } else if(c == 'e') {
-                if(hasExp || !hasNum) 
-                    return false;
-                hasExp = true;
-                hasNum = false;
-            } else if(c == '.') {
-                if(hasExp || hasDot) 
-                    return false;
-                hasDot = true;
-            } else
-                return false;
-            start++;
-        }
-        return hasNum;
-    }*/
-    
     public static boolean isNumber(String s){
         s = s.trim();
         if (s.length() == 0) return false;
