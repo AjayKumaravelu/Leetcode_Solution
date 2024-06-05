269578b3e25e3a97a22bd39e970e4a60216f665c
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-05 18:58:52 +0530

    Time: 5 ms (47.07%) | Memory: 49 MB (5.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       if(s == null || s.length() == 0){
-           return true;
-       } 
-       s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-       int left = 0;
-       int right = s.length() - 1;
-       while(left < right){
-           if(s.charAt(left) != s.charAt(right)){
-               return false;
-           }
-           left++;
-           right--;
-       }
-       return true;
+        int i = 0;
+        StringBuilder filteredString = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filteredString.append(Character.toLowerCase(c));
+            }
+        }
+        String r = filteredString.toString();
+
+        if (r.isEmpty()) {
+            return true;
+        }
+        
+        return Pallindrome(i,r);
+    }
+    public boolean Pallindrome(int i, String s){
+        if(i > s.length()/2){
+            return true;
+        }
+        if(s.charAt(i) != s.charAt(s.length() - i - 1)){
+            return false;
+        }
+        return Pallindrome(i+1, s);
     }
 }
