fb796b7b2c642922049aa250369e58b87cb0b63d
AjayKumaravelu <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-09 13:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (91.80%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int count_drop = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[(i + 1) % nums.length] >= nums[i]){
+                 
+            }
+            else{
+                count_drop++;
+            }
+        }
+        if(count_drop <= 1)return true;
+        else return false;  
+
+        // int dropCount = 0;
+        // int n = nums.length;
+
+        // // Traverse the array to count the number of drops
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] > nums[(i + 1) % n]) {
+        //         dropCount++;
+        //     }
+        // }
+
+        // // If the drop count is more than 1, the array is not sorted and rotated
+        // // If the drop count is 0 or 1, the array is sorted and rotated
+        // return dropCount <= 1;
+    }    
+}
