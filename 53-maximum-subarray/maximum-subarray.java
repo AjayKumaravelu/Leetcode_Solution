311dd2068cd1d46586628bd4b18be06b5e0d48ca
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-13 19:46:05 +0530

    Time: 1 ms (99.53%) | Memory: 57.3 MB (17.42%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,10 +3,20 @@ class Solution {
         // Using Kadanes Algorithm;
         int max = Integer.MIN_VALUE;
         int sum = 0;
+        // int ansStart = -1;
+        // int ansEnd = -1;
+        // int start = 0;
+        
         for(int i = 0; i < nums.length; i++){
+            // for finding the start and end index
+            // if(sum == 0){
+            //     start = i;
+            // }
             sum += nums[i];
             if(sum > max){
                 max = sum;
+                // ansStart = start;
+                // ansEnd = i;
             }
             if(sum < 0){
                 sum = 0;
