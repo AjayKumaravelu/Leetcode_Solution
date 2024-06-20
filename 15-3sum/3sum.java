c72790e6cccbac305c1183a42ae04cdccae9b740
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-20 13:10:37 +0530

    Time: 31 ms (61.37%) | Memory: 51 MB (94.64%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);  // Sort the array
-        List<List<Integer>> triplets = new ArrayList<>();
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;  // Skip duplicates
-            }
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int total = nums[i] + nums[left] + nums[right];
-                if (total == 0) {
-                    triplets.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;  // Skip duplicates
-                    }
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;  // Skip duplicates
-                    }
-                } else if (total < 0) {
-                    left++;
-                } else {
-                    right--;
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        // i kind of constant
+        for(int i = 0; i < nums.length; i++){
+            if(i != 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k){
+                long sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
-        return triplets;
+
+        return ans;
+ 
     }
 }
