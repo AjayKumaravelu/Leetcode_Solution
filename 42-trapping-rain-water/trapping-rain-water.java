8d31fe9b8cdf566c3efb98b4fac90484178527f7
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-21 09:11:15 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (27.85%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,22 +1,35 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
+        // Initialising the left and right pointer
+        int left = 0;
+        int right = height.length - 1;
+
+        // Initialise leftmax rightmax and res
+        int leftmax = 0;
+        int rightmax = 0;
         int res = 0;
-        // Let us iterate through the all height from second to secondlast height
-        for(int i = 1; i < n-1; i++){
-            // We are finding the maximum height on left, first we are intialising left
-            int left = height[i];
-            for(int j = 0 ; j < i ; j++){
-                left = Math.max(left, height[j]);
+
+        while(left <= right){
+            // Condition for going left and right 
+            // We make sure the element on right have someone greater or equal to left
+            if(height[left] <= height[right]){
+                if(height[left] >= leftmax){
+                    leftmax = height[left];
+                }else{
+                    res += leftmax - height[left];
+                }
+                left++;
             }
-            // We are finding the maximum height on right, first we are intialising right
-            int right = height[i];
-            for(int j = i + 1; j < n; j++){
-                right = Math.max(right, height[j]);
+            else{
+                if(height[right] >= rightmax){
+                    rightmax = height[right];
+                }else{
+                    res += rightmax - height[right];
+                }
+                right--;
             }
-            // We are adding the result with the minimum of left and right and subtracting the height of the current bar/index
-            res += Math.min(left, right) - height[i];
         }
+
         return res;
     }
 }
