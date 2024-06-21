6a4a0a650b1be146cdc43a5c956abe72e7136ff4
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-21 09:41:10 +0530

    Time: 1 ms (78.52%) | Memory: 44.4 MB (90.91%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,11 +3,14 @@ class Solution {
         // Using two pointer approach
         int i = 0;
         for(int j = 1; j < nums.length; j++){
+            // We will check whether the element which is same or not
             if(nums[j] != nums[i]){
                 nums[i+1] = nums[j];
                 i++;
             }
         }
+
         return i+1;
+
     }
 }
