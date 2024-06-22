9c3a1acfefe5b1933258493fe9580061e1b89888
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-22 21:56:46 +0530

    Time: 7 ms (96.36%) | Memory: 57 MB (24.80%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,27 +1,32 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), s, 0);
-        return result;
+        List<List<String>> ans = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        func(0,s,ans,path);
+        return ans;
     }
 
-    private void backtrack(List<List<String>> result, List<String> tempList, String s, int start) {
-        if (start == s.length()) {
-            result.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = start; i < s.length(); i++) {
-                if (isPalindrome(s, start, i)) {
-                    tempList.add(s.substring(start, i + 1));
-                    backtrack(result, tempList, s, i + 1);
-                    tempList.remove(tempList.size() - 1);
-                }
+    public void func(int index, String s, List<List<String>> ans, List<String> path){
+        if(index == s.length()){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i = index; i < s.length(); i++){
+            if(isPallindrome(s,index,i)){
+                //we will add substring
+                path.add(s.substring(index, i+1));
+                //We will call the recursive function
+                func(i + 1, s, ans, path);
+                //We will remove the last element as we backtrack
+                path.remove(path.size() - 1);
             }
         }
     }
 
-    private boolean isPalindrome(String s, int low, int high) {
-        while (low < high) {
-            if (s.charAt(low++) != s.charAt(high--)) {
+    public boolean isPallindrome(String s, int start, int end){
+        while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)){
                 return false;
             }
         }
