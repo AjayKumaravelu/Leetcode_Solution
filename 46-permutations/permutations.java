9d01960ed8ef1f5716d725fd782814919892add2
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-23 12:19:12 +0530

    Time: 1 ms (93.57%) | Memory: 44.3 MB (71.51%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,73 +1,33 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        List<List<Integer>> result = new ArrayList<>();
-        generatePermutation(nums,left,right,result);
-        return result;
-    }
-
-    public static void generatePermutation(int[] nums,int left ,int right,List<List<Integer>> result){
-        if(left == right){
-            printArray(nums,result);
-        }
-        else{
-            for(int i = left ; i <= right ; i++){
-                swap(nums,left,i);
-                generatePermutation(nums,left+1,right,result);
-                swap(nums,left,i);
+    public void perm(int index, int[] nums, List<List<Integer>> ans){
+        if(index == nums.length){
+            // copy ds to ans;
+            List<Integer> ds = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++){
+                ds.add(nums[i]);
             }
+
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        // We will traverse from index to n - 1
+        for(int i = index; i < nums.length; i++){
+            swap(i, index, nums);
+            perm(index + 1, nums, ans);
+            swap(i, index, nums);
         }
     }
 
-    public static void swap(int[] nums, int i, int j){
+    public void swap(int i, int j, int[] nums){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    public static void printArray(int[] nums,List<List<Integer>> result){
-        List<Integer> permutation = new ArrayList<>();
-        for(int num: nums){
-            permutation.add(num);
-        }
-        result.add(permutation);
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        perm(0,nums,ans);
+        return ans;    
     }
 }
-
- 
-
-/*class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        generatePermutation(nums, 0, result);
-        return result;
-    }
-
-    public static void generatePermutation(int[] nums, int index, List<List<Integer>> result){
-        if(index == nums.length - 1){
-            addToList(nums, result);
-        }
-        else{
-            for(int i = index; i < nums.length; i++){
-                swap(nums, index, i);
-                generatePermutation(nums, index + 1, result);
-                swap(nums, index, i);
-            }
-        }
-    }
-
-    public static void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    public static void addToList(int[] nums, List<List<Integer>> result){
-        List<Integer> permutation = new ArrayList<>();
-        for(int num: nums){
-            permutation.add(num);
-        }
-        result.add(permutation);
-    }
-}*/
