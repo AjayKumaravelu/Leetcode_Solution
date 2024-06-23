59f53d2fa669021bc6669d5aa67fb6cb93f0ef26
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-23 20:00:26 +0530

    Time: 11 ms (38.61%) | Memory: 40.6 MB (84.47%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -6,16 +6,19 @@ class Solution {
     }
 
     private boolean solve(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1'; c <= '9'; c++){
+                        if(isValid(board,i,j,c)){
                             board[i][j] = c;
-                            if (solve(board))
+
+                            if(solve(board) == true){
                                 return true;
-                            else
-                                board[i][j] = '.'; // backtrack
+                            } //else backtrack if it false
+                            else{
+                                board[i][j] = '.';
+                            }
                         }
                     }
                     return false;
@@ -26,14 +29,19 @@ class Solution {
     }
 
     private boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] != '.' && board[i][col] == c)
+        for(int i = 0; i < board.length; i++){
+            // we are traversing row
+            if(board[row][i] == c){
                 return false;
-            if (board[row][i] != '.' && board[row][i] == c)
+            }
+            // we are traversing column
+            if(board[i][col] == c){
                 return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.' &&
-                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+            }
+            // We are checking 3*3 matrix
+            if(board[3 * (row/3) + i/3][3 * (col/3) + i%3] == c){
                 return false;
+            }
         }
         return true;
     }
