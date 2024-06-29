5d47439905febfe352e06ef53f6aaaffceb39452
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-29 18:17:02 +0530

    Time: 5 ms (45.97%) | Memory: 44.6 MB (26.20%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,9 +1,11 @@
 class Solution {
     public String intToRoman(int num) {
         String ans = "";
+        // We will add the relevant 900 etc from other symbol
         String[] symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int[] value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
+        // We will traverse the num by position
         for(int pos = 0; pos < value.length; pos++){
             while(num >= value[pos]){
                 ans += symbol[pos];
