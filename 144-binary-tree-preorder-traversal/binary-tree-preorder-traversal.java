17f90df0d24373748676395a95942223a9e32185
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-30 21:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (69.68%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -25,10 +25,46 @@ class Solution {
         preorder(root.right, arr);
     }
 
-    public List<Integer> preorderTraversal(TreeNode root) {
+    /*public List<Integer> preorderTraversal(TreeNode root) {
         // preorder Traversal --->  root left right
         List<Integer> arr = new ArrayList<>();
         preorder(root, arr);
         return arr;
+    }*/
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preorder = new ArrayList<>();
+
+        // pointer to the current node
+        TreeNode curr = root;
+
+        while(curr != null){
+            // Case 1 when the left is null
+            if(curr.left == null){
+                preorder.add(curr.val);
+                curr = curr.right;
+            }
+            // Case 2 when there is left we will go to the rightmost element and point it o current and move the current
+            else{
+                TreeNode prev = curr.left;
+                // We are checking whether it is threaded
+                while(prev.right != null && prev.right != curr){
+                    prev = prev.right;
+                }
+
+                // previous right is null
+                if(prev.right == null){
+                    prev.right = curr;
+                    preorder.add(curr.val);
+                    curr = curr.left;
+                }
+                // if not null that is connected to current then we will break the thread
+                else{
+                    prev.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+        return preorder;
     }
 }
