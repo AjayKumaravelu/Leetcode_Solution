c915d55bb5dcb0bb50fd5eb266215af6bdb1b483
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-06-30 21:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (85.10%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -24,11 +24,47 @@ class Solution {
         arr.add(root.val);
         inorder(root.right, arr);
     }
-    public List<Integer> inorderTraversal(TreeNode root) {
+    /*public List<Integer> inorderTraversal(TreeNode root) {
         // Inorder Traversal ---> left root right
         List<Integer> arr = new ArrayList<>();
         inorder(root, arr);
         return arr;
         
+    }*/
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inorder = new ArrayList<>();
+
+        // pointer to the current node
+        TreeNode curr = root;
+
+        while(curr != null){
+            // Case 1 when the left is null
+            if(curr.left == null){
+                inorder.add(curr.val);
+                curr = curr.right;
+            }
+            // Case 2 when there is left we will go to the rightmost element and point it o current and move the current
+            else{
+                TreeNode prev = curr.left;
+                // We are checking whether it is threaded
+                while(prev.right != null && prev.right != curr){
+                    prev = prev.right;
+                }
+
+                // previous right is null
+                if(prev.right == null){
+                    prev.right = curr;
+                    curr = curr.left;
+                }
+                // if not null that is connected to current then we will break the thread
+                else{
+                    prev.right = null;
+                    inorder.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return inorder;
     }
 }
