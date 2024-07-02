0a6c93b08e50a070d40c2a3a710235f8b0662642
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-07-02 10:47:46 +0530

    Time: 2 ms (5.08%) | Memory: 43 MB (7.78%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // public int maxDepth(TreeNode root) {
+    //     if(root == null){
+    //         return 0;
+    //     }
+
+    //     int lh = maxDepth(root.left);
+    //     int rh = maxDepth(root.right);
+
+    //     return 1 + Math.max(lh, rh);
+    // }
+
+    public int maxDepth(TreeNode root) {
+        // We will use queue data structure
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        //List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        int level = 0;
+        if(root == null){
+            return level;
+        }
+
+        // we will push the root first
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int levelnum = queue.size();
+            List<Integer> sublist = new LinkedList<Integer>();
+            // we will traverse till levelnum
+            for(int i = 0; i < levelnum; i++){
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                sublist.add(queue.poll().val);
+            }
+            //wrapList.add(sublist);
+            level++;
+        }
+        return level;
+    }
+}
