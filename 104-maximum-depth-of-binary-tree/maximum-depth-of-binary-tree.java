1226274e87285bc3e3cc176ca422714780de29bc
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-07-02 10:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (64.72%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,41 +14,42 @@
  * }
  */
 class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+
+        return 1 + Math.max(lh, rh);
+    }
+
+    // using level order traversal
     // public int maxDepth(TreeNode root) {
+    //     // We will use queue data structure
+    //     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    //     //List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+    //     int level = 0;
     //     if(root == null){
-    //         return 0;
+    //         return level;
     //     }
 
-    //     int lh = maxDepth(root.left);
-    //     int rh = maxDepth(root.right);
+    //     // we will push the root first
+    //     queue.offer(root);
 
-    //     return 1 + Math.max(lh, rh);
-    // }
-
-    public int maxDepth(TreeNode root) {
-        // We will use queue data structure
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        //List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
-        int level = 0;
-        if(root == null){
-            return level;
-        }
-
-        // we will push the root first
-        queue.offer(root);
-
-        while(!queue.isEmpty()){
-            int levelnum = queue.size();
-            List<Integer> sublist = new LinkedList<Integer>();
-            // we will traverse till levelnum
-            for(int i = 0; i < levelnum; i++){
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                sublist.add(queue.poll().val);
-            }
-            //wrapList.add(sublist);
-            level++;
-        }
-        return level;
-    }
+    //     while(!queue.isEmpty()){
+    //         int levelnum = queue.size();
+    //         List<Integer> sublist = new LinkedList<Integer>();
+    //         // we will traverse till levelnum
+    //         for(int i = 0; i < levelnum; i++){
+    //             if(queue.peek().left != null) queue.offer(queue.peek().left);
+    //             if(queue.peek().right != null) queue.offer(queue.peek().right);
+    //             sublist.add(queue.poll().val);
+    //         }
+    //         //wrapList.add(sublist);
+    //         level++;
+    //     }
+    //     return level;
+    //}
 }
