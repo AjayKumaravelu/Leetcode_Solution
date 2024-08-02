202fc4306035cd4dc9ecd4321d0176f4e1bd445c
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-08-02 10:22:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (99.72%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -15,23 +15,25 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        if(root == null){
-            return;
+        TreeNode curr = root;
+
+        // We will traverse until the curr becomes null
+        while(curr != null){
+                if(curr.left != null){
+                    TreeNode prev = curr.left;
+
+                    // We will traverse untill the last node in left subtree 
+                    while(prev.right != null){
+                        prev = prev.right;
+                    }
+
+                    // Then we will point the prev.right  to the curr.right and curr.right to curr.left and we have to make cuur.left == null
+                    prev.right = curr.right;
+                    curr.right = curr.left;
+                    curr.left = null;
+                }
+                curr = curr.right;
         }
 
-        flatten(root.left);
-        flatten(root.right);
-
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-
-        root.left = null;
-        root.right = left;
-
-        TreeNode current = root;
-        while(current.right != null){
-            current = current.right;
-        }
-        current.right = right;
     }
 }
