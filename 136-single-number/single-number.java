aba4c264211f9e0d29d88e549c0a7be5f2c91c38
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-08-17 11:49:43 +0530

    Time: 1 ms (99.85%) | Memory: 46.5 MB (5.58%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,27 +1,27 @@
 class Solution {
-    // public int singleNumber(int[] nums) {
-    //     // Using XOR method for optimal 
-    //     int n = nums.length;
+    public int singleNumber(int[] nums) {
+        // Using XOR method for optimal 
+        int n = nums.length;
 
-    //     int XOR = 0;
-    //     for(int i = 0; i < nums.length; i++){
-    //         XOR = XOR ^ nums[i];
-    //     }
-    //     return XOR;
-    // }
-
-    public int singleNumber(int[] nums){
-        HashMap<Integer, Integer> map = new HashMap<>();
-
+        int XOR = 0;
         for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            XOR = XOR ^ nums[i];
         }
-
-        for(Map.Entry<Integer, Integer> it: map.entrySet()){
-            if(it.getValue() == 1){
-                return it.getKey();
-            }
-        }
-        return -1;
+        return XOR;
     }
+
+    // public int singleNumber(int[] nums){
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+
+    //     for(int i = 0; i < nums.length; i++){
+    //         map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+    //     }
+
+    //     for(Map.Entry<Integer, Integer> it: map.entrySet()){
+    //         if(it.getValue() == 1){
+    //             return it.getKey();
+    //         }
+    //     }
+    //     return -1;
+    // }
 }
