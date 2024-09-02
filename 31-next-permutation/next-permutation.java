4fffbaa5be703aa619b38c84023e54e589ffa07d
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-09-02 21:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (25.68%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,32 +1,32 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-         int n = nums.length;
-         // Step 1 :  We are going to find the break point in nums array
-         // As the breakpoint can happen at the position minimally on n-2
-         int ind = -1;
-         for(int i = n-2; i >= 0; i--){
-            if(nums[i] < nums[i+1]){
+        int n = nums.length;
+
+        // Step 1: Find the break point
+        int ind = -1;
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] < nums[i + 1]){
                 ind = i;
                 break;
             }
-         }
+        }
 
-         // edge case
-         if(ind == -1){
-            reverse(nums, 0, n-1);
+        // If no break point exists, reverse the entire array
+        if(ind == -1){
+            reverse(nums, 0, n - 1);
             return;
-         }
+        }
 
-         // Step 2 : We are going to find the number which is smallest greater than the breakpoint (ie: ind)
-         for(int i = n - 1; i > ind; i--){
+        // Step 2: Find the next greater element in the right half
+        for(int i = n - 1; i > ind; i--){
             if(nums[i] > nums[ind]){
                 swap(nums, i, ind);
-                break;
+                break; // Important to break after swap
             }
-         }
+        }
 
-         // Step 3: Sort/reverse the remaining array to make the nnext Permutation minimum
-        reverse(nums, ind+1, n-1);
+        // Step 3: Reverse the right half
+        reverse(nums, ind + 1, n - 1);
     }
     
     private void swap(int[] nums, int i, int j) {
@@ -37,7 +37,7 @@ class Solution {
 
     private void reverse(int[] nums, int start, int end){
         while(start < end){
-            swap(nums,start,end);
+            swap(nums, start, end);
             start++;
             end--;
         }
