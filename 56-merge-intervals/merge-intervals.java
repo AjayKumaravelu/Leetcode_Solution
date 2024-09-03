cd41b8e670a9f3e75964ab5e6392c104ff9ca76d
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2024-09-03 19:46:46 +0530

    Time: 11 ms (15.69%) | Memory: 46.3 MB (75.09%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,19 +1,29 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-        if(n == 0){
+        if (n == 0) {
             return new int[0][];
         }
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
-        List<int[]> ans = new ArrayList<>();
-        for(int[] interval: intervals){
-            if(ans.isEmpty() || ans.get(ans.size()-1)[1] < interval[0]){
-                ans.add(interval);
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
             }
-            else{
-                ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1], interval[1]);
+        });
+
+        List<int[]> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            // case 1: when it is empty or it does not overlap
+            if (ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1)[1]) {
+                ans.add(new int[]{intervals[i][0], intervals[i][1]});
+            }
+            // case 2: if it overlaps with the previous interval
+            else {
+                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
             }
         }
+
         return ans.toArray(new int[ans.size()][]);
     }
 }
