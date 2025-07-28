e5326805131df289fd95c8a3601dc0e29c882fc7
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2025-07-28 20:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (10.80%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,43 +1,57 @@
-class Solution {
-    public boolean check1(int[] nums) {
-        // int count_drop = 0;
-        // for(int i = 0; i < nums.length; i++){
-        //     if(nums[(i + 1) % nums.length] >= nums[i]){
+// class Solution {
+//     public boolean check1(int[] nums) {
+//         // int count_drop = 0;
+//         // for(int i = 0; i < nums.length; i++){
+//         //     if(nums[(i + 1) % nums.length] >= nums[i]){
                  
-        //     }
-        //     else{
-        //         count_drop++;
-        //     }
-        // }
-        // if(count_drop <= 1)return true;
-        // else return false;  
+//         //     }
+//         //     else{
+//         //         count_drop++;
+//         //     }
+//         // }
+//         // if(count_drop <= 1)return true;
+//         // else return false;  
 
+//         int dropCount = 0;
+//         int n = nums.length;
+
+//         // Traverse the array to count the number of drops
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] > nums[(i + 1) % n]) {
+//                 dropCount++;
+//             }
+//         }
+
+//         // If the drop count is more than 1, the array is not sorted and rotated
+//         // If the drop count is 0 or 1, the array is sorted and rotated
+//         return dropCount <= 1;
+//     } 
+
+
+//     public boolean check(int[] nums){
+//         int drop = 0;
+//         int n = nums.length;
+
+//         for(int i = 0; i < n; i++){
+//             if(nums[i] > nums[(i + 1) % n]){
+//                 drop++;
+//             }
+//         }
+
+//         return drop <= 1;
+//     }   
+// }
+
+
+class Solution{
+    public boolean check(int[] nums){
         int dropCount = 0;
-        int n = nums.length;
-
-        // Traverse the array to count the number of drops
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % n]) {
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > nums[(i+1) % nums.length]){
                 dropCount++;
             }
         }
 
-        // If the drop count is more than 1, the array is not sorted and rotated
-        // If the drop count is 0 or 1, the array is sorted and rotated
         return dropCount <= 1;
-    } 
-
-
-    public boolean check(int[] nums){
-        int drop = 0;
-        int n = nums.length;
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] > nums[(i + 1) % n]){
-                drop++;
-            }
-        }
-
-        return drop <= 1;
-    }   
+    }
 }
