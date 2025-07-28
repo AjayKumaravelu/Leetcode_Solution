84e9bd35df28629fea5be667373fcfb3ea140f27
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2025-07-28 21:07:11 +0530

    Time: 203 ms (5.45%) | Memory: 57.7 MB (13.88%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,21 +1,48 @@
-class Solution {
-    public void rotate(int[] nums, int k) {
-        int[] result = new int[nums.length];
-        int n = nums.length;
-        k = k % n;
-        for (int i = 0 ; i < k ; i++){
-            result[i] = nums[n - k + i];
-        }
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         int[] result = new int[nums.length];
+//         int n = nums.length;
+//         k = k % n;
+//         for (int i = 0 ; i < k ; i++){
+//             result[i] = nums[n - k + i];
+//         }
  
-        for(int i = 0; i < n - k ; i++){
-            result[k + i] = nums[i];
-        }
+//         for(int i = 0; i < n - k ; i++){
+//             result[k + i] = nums[i];
+//         }
         
-        for(int i = 0; i < n; i++){
-            nums[i] = result[i];
+//         for(int i = 0; i < n; i++){
+//             nums[i] = result[i];
+//         }
+
+//         for(int i = 0; i < nums.length ; i++){
+//             System.out.print(nums[i] + " ");
+//         }
+//     }
+// }
+
+class Solution {
+    public void rotate(int[] nums, int k){
+        int n = nums.length;
+        int[] res = new int[n];
+        
+        k = k % n;
+        int j = 0;
+        for(int i = n - k; i < n; i++){
+            res[j] = nums[i];
+            j++;
         }
 
-        for(int i = 0; i < nums.length ; i++){
+        for(int i = 0; i < n - k; i++){
+            res[j] = nums[i];
+            j++;
+        }
+
+        for(int i = 0; i < n; i++){
+            nums[i] = res[i];
+        }
+
+        for(int i = 0; i < n ; i++){
             System.out.print(nums[i] + " ");
         }
     }
