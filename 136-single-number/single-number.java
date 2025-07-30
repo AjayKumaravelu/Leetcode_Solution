29739dcc8311ec4087523b2415ad4931e5b0b785
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2025-07-30 20:54:02 +0530

    Time: 13 ms (18.93%) | Memory: 44.9 MB (96.80%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int singleNumber(int[] nums) {
+    public int singleNumber1(int[] nums) {
         // Using XOR method for optimal 
         int n = nums.length;
 
@@ -8,6 +8,25 @@ class Solution {
             XOR = XOR ^ nums[i];
         }
         return XOR;
+    }
+
+    public int singleNumber(int[] nums){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int value = map.getOrDefault(nums[i], 0);
+            map.put(nums[i], value+1);
+        }
+
+        for(Map.Entry<Integer, Integer> it: map.entrySet()){
+            if(it.getValue() == 1){
+                return it.getKey();
+            }
+        }
+
+        return -1;
+
+
     }
 
     // public int singleNumber(int[] nums){
