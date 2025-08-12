f7d501a35ad09fd20e1daa1edda0a71e1443821d
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2025-08-12 22:05:57 +0530

    Time: 10 ms (20.92%) | Memory: 46.6 MB (72.92%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -13,13 +13,22 @@ class Solution {
 
         List<int[]> ans = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            // case 1: when it is empty or it does not overlap
-            if (ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1)[1]) {
+        // for (int i = 0; i < n; i++) {
+        //     // case 1: when it is empty or it does not overlap
+        //     if (ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1)[1]) {
+        //         ans.add(new int[]{intervals[i][0], intervals[i][1]});
+        //     }
+        //     // case 2: if it overlaps with the previous interval
+        //     else {
+        //         ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
+        //     }
+        // }
+
+
+        for(int i = 0; i < n; i++){
+            if(ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1)[1]){
                 ans.add(new int[]{intervals[i][0], intervals[i][1]});
-            }
-            // case 2: if it overlaps with the previous interval
-            else {
+            }else{
                 ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
             }
         }
