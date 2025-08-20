9ee82013eed9e4960f34f4c78b56165d772d7611
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2025-08-20 22:15:47 +0530

    Time: 1 ms (91.82%) | Memory: 45 MB (54.48%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> levelOrder1(TreeNode root) {
         // We will use queue data structure
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
@@ -38,5 +38,43 @@ class Solution {
             wrapList.add(sublist);
         }
         return wrapList;
+    }
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // Answer stores the answer level by level in 2d array
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // Edge cases
+        if(root == null){
+            return ans;
+        }
+
+        // We need Queue data Structure to keep track of the node 
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
+                level.add(node.val);
+
+                if(node.left != null){
+                    q.add(node.left);
+                }
+
+                if(node.right != null){
+                    q.add(node.right);
+                }
+            }
+            ans.add(level);
+
+        }
+
+        return ans;
+
     }
 }
