942f654f1615a480ee8ba51127ddff60274c2552
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2025-08-29 19:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (66.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -16,15 +16,26 @@ class Solution {
         // return dp[n] = climbStairs(n-2) + climbStairs(n-1);
 
         // Tabulation --> Top Down Approach
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0] = 1;
-        dp[1] = 1;
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp,-1);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i = 2; i <= n; i++){
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }
+        // return dp[n];
+
+        // Optimised Approach
+        int prev2 = 1;
+        int prev = 1;
+        int curr_i = 1;
 
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            curr_i = prev2 + prev;
+            prev2 = prev;
+            prev = curr_i;
         }
 
-        return dp[n];
+        return curr_i;
     }
 }
