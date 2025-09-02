ed4ffde90efc4a58a03002a2ec6b76cfcd6b9583
Ajay07 <153514250+AjayKumaravelu@users.noreply.github.com>
2025-09-02 19:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (25.79%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,14 +1,44 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int N = n + m - 2;
-        int R = m - 1;
-        double result = 1;
+    public int uniquePaths(int m, int n){
+        int[][] dp = new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        return uniquePathsMemoization(m-1, n-1, dp);
+    }
+    public int uniquePathsRecursive(int m, int n) {
+        // Using recursion
+        // Base Condition
+        if(m == 0 && n == 0) return 1; // We found the final destination
+        if(m < 0 || n < 0) return 0; // We went outside boundary;
 
-        // finding combination
-        for(int i = 1; i <= R; i++){
-            result = result * (N -  R + i)/i;
-        }
+        int up = uniquePathsRecursive(m-1, n);
+        int left = uniquePathsRecursive(m, n-1);
 
-        return (int)result;
+        return up + left;
+        // int N = n + m - 2;
+        // int R = m - 1;
+        // double result = 1;
+
+        // // finding combination
+        // for(int i = 1; i <= R; i++){
+        //     result = result * (N -  R + i)/i;
+        // }
+
+        // return (int)result;
+    }
+
+    public int uniquePathsMemoization(int i, int j, int[][] dp){
+        // Base Condition
+        if(i == 0 && j == 0) return 1;
+
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = uniquePathsMemoization(i-1, j , dp);
+        int left = uniquePathsMemoization(i, j-1, dp);
+
+        return dp[i][j] = up + left;
+
     }
 }
